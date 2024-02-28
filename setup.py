de60b040c7038f01e888a28f55c20facd38124cd
yanxinle <1020121123@qq.com>
2024-02-28 12:51:45 +0800

    更新setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LeleTestComm",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib"
+    ],
+    author="YanXinle",
+    author_email="1020121123@qq.com",
+    description="绘制基本图形",
+
+    url="https://github.com/Yanxinle1123/LeleDrawDesigns",
+)
