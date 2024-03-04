7f13b668f73d3e3c10cc4b4c2d0978a94505b881
yanxinle <1020121123@qq.com>
2024-03-04 21:45:39 +0800

    更新histogram和test文件

--- LeleDrawDesigns/test.py
+++ LeleDrawDesigns/test.py
@@ -2,15 +2,18 @@ from LeleDrawDesigns.histogram import Histogram
 from LeleDrawDesigns.line_chart import LineChart, obtain_all_skin
 
 curved_line_chart = LineChart(input_values=[1, 2, 3, 4, 5], squares=[1, 4, 9, 16, 25], style='curved',
-                              skin='Solarize_Light2', set_title='CurvedLineChart', set_title_fontsize=20,
+                              line_color='green', skin='Solarize_Light2', set_title='CurvedLineChart',
+                              set_title_fontsize=20,
                               set_x_label='XLabel', set_x_label_fontsize=20, set_y_label='YLabel',
                               set_y_label_fontsize=20, line_width=3)
 
 straight_line_chart = LineChart(input_values=[1, 2, 3, 4, 5], squares=[1, 4, 9, 16, 25], style='straight',
-                                skin='Solarize_Light2', set_title='StraightLineChart', set_title_fontsize=20,
+                                line_color='green', skin='Solarize_Light2', set_title='StraightLineChart',
+                                set_title_fontsize=20,
                                 set_x_label='XLabel', set_x_label_fontsize=20, set_y_label='YLabel',
                                 set_y_label_fontsize=20, line_width=3)
 
 obtain_all_skin()
 
-histogram = Histogram()
+histogram = Histogram(title='Histogram', title_fontsize=20, x_label='XLabel', x_label_fontsize=20, y_label='YLabel',
+                      y_label_fontsize=20, color='green', edgecolor='black')
