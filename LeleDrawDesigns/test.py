3cc609e12b49a68459d86b3fbc3c2244dff60085
yanxinle <1020121123@qq.com>
2024-03-04 13:31:28 +0800

    更新test文件

--- LeleDrawDesigns/test.py
+++ LeleDrawDesigns/test.py
@@ -2,12 +2,12 @@ from LeleDrawDesigns.line_chart import LineChart, obtain_all_skin
 
 curved_line_chart = LineChart(input_values=[1, 2, 3, 4, 5], squares=[1, 4, 9, 16, 25], style='curved',
                               skin='Solarize_Light2', set_title='CurvedLineChart', set_title_fontsize=20,
-                              set_xlabel='XLabel', set_xlabel_fontsize=20, set_ylabel='YLabel',
-                              set_ylabel_fontsize=20, linewidth=3)
+                              set_x_label='XLabel', set_x_label_fontsize=20, set_y_label='YLabel',
+                              set_y_label_fontsize=20, line_width=3)
 
 straight_line_chart = LineChart(input_values=[1, 2, 3, 4, 5], squares=[1, 4, 9, 16, 25], style='straight',
                                 skin='Solarize_Light2', set_title='StraightLineChart', set_title_fontsize=20,
-                                set_xlabel='XLabel', set_xlabel_fontsize=20, set_ylabel='YLabel',
-                                set_ylabel_fontsize=20, linewidth=3)
+                                set_x_label='XLabel', set_x_label_fontsize=20, set_y_label='YLabel',
+                                set_y_label_fontsize=20, line_width=3)
 
 obtain_all_skin()
