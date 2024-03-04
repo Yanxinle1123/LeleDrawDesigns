4c439e792f89ca9cb4226f1d4a9c1b07ef7ec9d9
yanxinle <1020121123@qq.com>
2024-03-04 17:20:29 +0800

    更新histogram文件

--- LeleDrawDesigns/histogram.py
+++ LeleDrawDesigns/histogram.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from die import Die
 
 die = Die()
@@ -18,17 +20,17 @@ print(list_frequencies)
 
 class Histogram:
     def __init__(self, input_values=None, frequencies=None, color=None, edgecolor=None, title=None, title_fontsize=None,
-                 xlabel=None, xlabel_fontsize=None, ylabel=None, ylabel_fontsize=None):
+                 x_label=None, x_label_fontsize=None, y_label=None, y_label_fontsize=None):
         self._input_values = input_values
         self._frequencies = frequencies
         self._color = color
         self._edgecolor = edgecolor
         self._title = title
         self._title_fontsize = title_fontsize
-        self._xlabel = xlabel
-        self._xlabel_fontsize = xlabel_fontsize
-        self._ylabel = ylabel
-        self._ylabel_fontsize = ylabel_fontsize
+        self._x_label = x_label
+        self._x_label_fontsize = x_label_fontsize
+        self._y_label = y_label
+        self._y_label_fontsize = y_label_fontsize
 
         if self._input_values is None:
             self._input_values = poss_results
@@ -38,3 +40,13 @@ class Histogram:
             self._color = 'blue'
         if self._edgecolor is None:
             self._edgecolor = 'black'
+
+        self.draw()
+
+    def draw(self):
+        plt.bar(self._input_values, self._frequencies, color=self._color, edgecolor=self._edgecolor)
+        plt.title(self._title, fontsize=self._title_fontsize)
+        plt.xlabel(self._x_label, fontsize=self._x_label_fontsize)
+        plt.ylabel(self._y_label, fontsize=self._y_label_fontsize)
+
+        plt.show()
--- LeleDrawDesigns/test.py
+++ LeleDrawDesigns/test.py
@@ -1,3 +1,4 @@
+from LeleDrawDesigns.histogram import Histogram
 from LeleDrawDesigns.line_chart import LineChart, obtain_all_skin
 
 curved_line_chart = LineChart(input_values=[1, 2, 3, 4, 5], squares=[1, 4, 9, 16, 25], style='curved',
@@ -11,3 +12,5 @@ straight_line_chart = LineChart(input_values=[1, 2, 3, 4, 5], squares=[1, 4, 9, 
                                 set_y_label_fontsize=20, line_width=3)
 
 obtain_all_skin()
+
+histogram = Histogram()
