c670123719ac0454e499411df07e12636b4a2b34
yanxinle <1020121123@qq.com>
2024-03-07 13:22:01 +0800

    更新setup文件和README文件, 并打包库为1.2.0版本

--- LeleDrawDesigns/test.py
+++ LeleDrawDesigns/test.py
@@ -17,24 +17,24 @@ xlabel = 'Result'
 ylabel = 'Frequency of Result'
 x_labels = ["one", "two", "three", "four", "five", "six"]
 
-histogram = Histogram(poss_results, frequencies, x_ticks=x_labels, x_ticks_fontsize=20, y_ticks_fontsize=50,
-                      skin='Solarize_Light2', title='Results of Rolling One D6 1,000 Times(Histogram)',
-                      title_fontsize=20, x_label=xlabel, y_label=ylabel, x_label_fontsize=20, y_label_fontsize=20,
-                      color='green', edgecolor='black', figsize=(10, 7),
-                      save_name='Results of Rolling One D6 1,000 Times(Histogram)')
+Histogram(poss_results, frequencies, x_ticks=x_labels, x_ticks_fontsize=20, y_ticks_fontsize=50,
+          skin='Solarize_Light2', title='Results of Rolling One D6 1,000 Times(Histogram)',
+          title_fontsize=20, x_label=xlabel, y_label=ylabel, x_label_fontsize=20, y_label_fontsize=20,
+          color='green', edgecolor='black', figsize=(10, 7),
+          save_name='Results of Rolling One D6 1,000 Times(Histogram)')
 
-straight_line_chart = LineChart(input_values=input_values, squares=frequencies, x_ticks=x_labels, style='straight',
-                                line_color='green', skin='Solarize_Light2',
-                                set_title='Results of Rolling One D6 1,000 Times(StraightLineChart)',
-                                set_title_fontsize=20, set_x_label=xlabel, set_x_label_fontsize=20, set_y_label=ylabel,
-                                set_y_label_fontsize=20, line_width=3, figsize=(10, 7), x_ticks_fontsize=20,
-                                save_name='Results of Rolling One D6 1,000 Times(StraightLineChart)')
+LineChart(input_values=input_values, squares=frequencies, x_ticks=x_labels, style='straight',
+          line_color='green', skin='Solarize_Light2',
+          set_title='Results of Rolling One D6 1,000 Times(StraightLineChart)',
+          set_title_fontsize=20, set_x_label=xlabel, set_x_label_fontsize=20, set_y_label=ylabel,
+          set_y_label_fontsize=20, line_width=3, figsize=(10, 7), x_ticks_fontsize=20,
+          save_name='Results of Rolling One D6 1,000 Times(StraightLineChart)')
 
-curved_line_chart = LineChart(input_values=input_values, squares=frequencies, x_ticks=x_labels, style='curved',
-                              line_color='green', skin='Solarize_Light2',
-                              set_title='Results of Rolling One D6 1,000 Times(CurvedLineChart)', set_title_fontsize=20,
-                              set_x_label=xlabel, set_x_label_fontsize=20, set_y_label=ylabel, set_y_label_fontsize=20,
-                              line_width=3, figsize=(10, 7), x_ticks_fontsize=20,
-                              save_name='Results of Rolling One D6 1,000 Times(CurvedLineChart)')
+LineChart(input_values=input_values, squares=frequencies, x_ticks=x_labels, style='curved',
+          line_color='green', skin='Solarize_Light2',
+          set_title='Results of Rolling One D6 1,000 Times(CurvedLineChart)', set_title_fontsize=20,
+          set_x_label=xlabel, set_x_label_fontsize=20, set_y_label=ylabel, set_y_label_fontsize=20,
+          line_width=3, figsize=(10, 7), x_ticks_fontsize=20,
+          save_name='Results of Rolling One D6 1,000 Times(CurvedLineChart)')
 
 obtain_all_skin()
